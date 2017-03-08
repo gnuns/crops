424f83c1db51751f20f3f9eda2b18c8b8e81129f
Gabriel <gabriel@multiverso.me>
2017-03-08 12:39:57 -0300

    Send with content-type header

--- crops.js
+++ crops.js
@@ -10,21 +10,26 @@ module.exports = { softCrop }
 
 function softCrop (req, res) {
   let params = req.params
+
+  res.set('X-Powered-By', 'Crops')
+
   if (params.w > MAX_CROP_WIDTH || params.h > MAX_CROP_HEIGHT) {
     res
     .status(500)
     .send(`Error! max width: ${MAX_CROP_WIDTH}, max height: ${MAX_CROP_HEIGHT}`)
   }
+
   downloadImage(params[0])
-  .then((imgPath) => {
-    gm(imgPath)
+  .then((img) => {
+    gm(img.path)
     .resize(params.w, params.h, '^')
     .gravity(req.query.gravity || 'Center')
     .crop(params.w, params.h)
     .stream((err, stdout, stderr) => {
       if (err) return res.status(500).send(err)
       try {
-        setTimeout(() => fs.unlink(imgPath), 1500)
+        setTimeout(() => fs.unlink(img.path), 1500)
+        res.set('Content-Type', img.type)
         stdout.pipe(res)
       } catch (e) {
         res.status(500).send(`Error! ${err}`)
@@ -49,7 +54,7 @@ function downloadImage (path) {
         try {
           fs.writeFile(file, response.body, 'binary', (err) => {
             if (err) return reject(err)
-            return resolve(file)
+            return resolve({path: file, type: response.headers['content-type']})
           })
         } catch (e) {
           return reject(e)
